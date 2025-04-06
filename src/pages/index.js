8cf0558a6845d6e814a472a627d19a3eeec93519
Alexmz1 <menez.alexis19@gmail.com>
2025-04-06 19:25:31 +0200

    Add sections in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import Footer from '@/components/footer';
 import Header from '../components/header';
+import Info from '@/components/info';
+import Composition from '@/components/composition';
+import Contact from '@/components/contact';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+
+      <Info />
+
+      <Composition />
+
+      <Contact />
 
       <Footer />
     </div>
