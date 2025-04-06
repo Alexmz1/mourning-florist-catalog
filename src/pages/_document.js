c38cac63f385eb2f821cae47b0e7deb6a6cd05ea
Alexmz1 <menez.alexis19@gmail.com>
2025-04-06 19:21:46 +0200

    Add name tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Fleurs de la Seine</title>
       <body>
         <Main />
         <NextScript />
